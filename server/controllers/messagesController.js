dc17605dabe719ed767feb0ca72b297829788181
Daniele Simas <83620485+danisimas@users.noreply.github.com>
2023-02-04 09:50:48 -0400

    Revert "funcioinando"
    This reverts commit 90d7220d1eac47a2a7a8b6be71f2378dfd1f1f7c.

--- server/controllers/messagesController.js
+++ server/controllers/messagesController.js
@@ -1,42 +1,11 @@
 const messageModel = require("../models/messageModel");
-const crypto = require('crypto-js');
 
-function generateRandomKey() {
-    let key = ''
-    let possible = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-    for (let i = 0; i < 16; i++) {
-      key += possible.charAt(Math.floor(Math.random() * possible.length))
-    }
-    // adicionar uma verificação de segurança para garantir que a string tenha pelo menos 1 caractere
-    if (key.length === 0) {
-        key = 'a';
-    }
-    return key
-}
+const crypto = require("crypto-js");
 
-function rc4Key(key) {
-    // codificar a string como UTF-8 antes de usá-la como chave RC4
-    let keyBuf = Buffer.from(key, 'utf8')
-    let s = []
-    for (let i = 0; i < 256; i++) {
-      s[i] = i
-    }
-    let j = 0
-    for (let i = 0; i < 256; i++) {
-      j = (j + s[i] + keyBuf[i % keyBuf.length]) % 256
-      let temp = s[i]
-      s[i] = s[j]
-      s[j] = temp
-    }
-    return s.toString()
-}
+const key = "chave_secreta";
 
-const keyRandom = generateRandomKey()
-let keyRC4 = rc4Key(keyRandom)
-const key = JSON.stringify({keyRC4})
 
-module.exports.addMessage = async (req, res, next) => { 
-
+module.exports.addMessage = async (req, res, next) => {
     try {
         const {from,to,message} = req.body;
         const data = await messageModel.create({
@@ -49,18 +18,15 @@ module.exports.addMessage = async (req, res, next) => {
             ],
             sender:from,
         });
-        console.log(keyRandom)
 
         if(data) return res.json({
             msg: "Message added successfully!"
         });
-
         return res.json({ 
             msg: "Failed to add message to DB"
         });
-       
-    } catch (err) {
 
+    } catch (err) {
         next(err);
     }
 };
@@ -76,12 +42,13 @@ module.exports.getAllMessage = async (req, res, next) => {
         const projectMessages = messages.map((msg)=>{
             return{
                 fromSelf: msg.sender.toString() === from,
-                message: crypto.RC4.decrypt( msg.message.text,key).toString(crypto.enc.Utf8)
+                message: crypto.RC4.decrypt(msg.message.text, key).toString(crypto.enc.Utf8)
             };
         });
-        console.log(keyRandom)
+
         res.json(projectMessages);
     } catch (error) {
         next(error);
     }
 };
+
